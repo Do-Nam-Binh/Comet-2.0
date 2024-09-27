f935f3c3877d815cffdf9baa9325d9b29f62af24
Vally <113294564+Needalife@users.noreply.github.com>
2024-09-27 09:24:48 +0700

    bot testing

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const {Client} = require("discord.js");
+const {Client, IntentsBitField} = require("discord.js");
 const {token} = require("./config.json");
 
-const client = new Client({intents: []});
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+});
+
+client.on('ready', (client) => {
+    console.log(`✅ ${client.user.displayName} is online ✅`);
+})
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    } else {
+        console.log(`${message.author.username}: ${message.content}`)
+    }
+})
 
 client.login(token);
 
