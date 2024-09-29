58929ef94428cc4f51c07842b15a3ac7e4b87481
Vally <113294564+Needalife@users.noreply.github.com>
2024-09-29 21:31:30 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,6 @@
-const { Client, GatewayIntentBits, REST, Routes } = require("discord.js");
-const { token, botId, serverId } = require("./config.json"); 
+const { Client, GatewayIntentBits } = require("discord.js");
+const { token } = require("./config.json"); 
 const eventHandler = require('./handlers/eventHandler');
-const fs = require('fs');
-const path = require('path');
 
 const client = new Client({
     intents: [
