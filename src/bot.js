de75b969aa1ce957ca2ae0c4902ffd693490b2e9
Vally <113294564+Needalife@users.noreply.github.com>
2024-09-29 17:44:48 +0700

    Bam

--- src/bot.js
+++ src/bot.js
@@ -32,7 +32,7 @@ const rest = new REST({ version: '10' }).setToken(token);
             { body: [...client.commands.values()].map(command => command.data.toJSON()) },
         );
 
-        console.log('Successfully');
+        console.log('Successfully load application (/) commands!');
     } catch (error) {
         console.error(error);
     }
