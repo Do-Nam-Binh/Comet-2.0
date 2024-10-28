3a2c8eb9f04efb48a6fe32c7ddbf51ab996bf8d7
Vally <113294564+Needalife@users.noreply.github.com>
2024-10-28 23:33:30 +0700

    testing maintenance mode

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -1,7 +1,7 @@
 const { ApplicationCommandOptionType,PermissionFlagsBits,} = require('discord.js');
 
 module.exports = {
-    deleted: true,
+    maintenance: true,
     name: 'ban',
     description: 'Bans a member!!!',
 
